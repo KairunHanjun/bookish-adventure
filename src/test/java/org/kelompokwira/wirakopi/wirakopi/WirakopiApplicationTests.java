d320fa1f6529b89a54e8d869b52fc11c236b2da7
KairunHanjun <ayamaja32@gmail.com>
2024-06-22 18:26:31 +0700

    No Test

--- src/test/java/org/kelompokwira/wirakopi/wirakopi/WirakopiApplicationTests.java
+++ src/test/java/org/kelompokwira/wirakopi/wirakopi/WirakopiApplicationTests.java
@@ -1,11 +1,5 @@
 package org.kelompokwira.wirakopi.wirakopi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 class WirakopiApplicationTests {
-    @Test
-	void contextLoads() {
-
-	}
+	
 }
